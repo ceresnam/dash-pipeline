f11f9c127bf3f1065b68a6581647bc3d2f58db17
ceresnam <michal.ceresna@gmail.com>
2021-02-03 21:14:04 +0000

    deploy - added service account as script param

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ${0} service_account_key.json"
+    exit 1
+fi
+
 # osx: export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 export PATH=$PATH:~/apache-maven-3.6.3/bin
+
+export GOOGLE_APPLICATION_CREDENTIALS=${1}
 
 mvn compile exec:java \
     -Dexec.mainClass=com.modrykonik.dash.DashPipeline \
@@ -13,5 +20,6 @@ mvn compile exec:java \
         --templateLocation=gs://dash_pipelines/templates/DashPipeline \
     "
 
+gcloud auth activate-service-account --key-file ${1}
 gsutil cp src/main/java/com/modrykonik/dash/DashPipeline_metadata gs://dash_pipelines/templates/
 
